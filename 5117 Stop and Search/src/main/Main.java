75f66be24311637ce0aa2cd514162b008d662d43
cmpjfors <cmpjfors@CMPB31.cms.livjm.ac.uk>
2018-10-10 11:04:34 +0100

    Initial Commit

---
+++ 5117 Stop and Search/src/main/Main.java
@@ -0,0 +1,30 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		TODO:
+//		•	List all Reasons For Search
+//		•	Display all the Stop&Search’s with a user specified Reason For Search
+//		•	Determine how many Stop&Search’s were:
+//			o	Successful 		(Punishment Related to Reason For Search)
+//			o	Partially Successful 	(Punishment Unrelated to Reason For Search)
+//			o	Unsuccessful 		(No Punishment)
+//		•	Determine which legislation produced the highest amount of Stop&Searches each month
+//		•	Determine which ethnic group is Stopped&Searched the most out of:
+//			o	By Month per Police Force (+ In Reverse Order by Date) 
+//			o	By Specified Legislation
+//		•	One More Feature that uses Searching/Sorting of our choosing (Multiple Search Attributes)
+//			o	Gender and Outcome
+//			o	Age Range and Legislation
+//			o	etc
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
