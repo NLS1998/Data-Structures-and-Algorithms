e073f2ebe642c3b9082ccb4cd349c70923560b10
Robert Rimmer <Robert Rimmer@LAPTOP-K9MUD67O>
2018-10-27 22:57:04 +0100

    reading in multiple files from a directory

--- 5117 Stop and Search/src/main/CSVReader.java
+++ 5117 Stop and Search/src/main/CSVReader.java
@@ -1,15 +1,40 @@
 package main;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
-public class CSVReader {
+public class CSVReader 
+{
+public static void main(String[] args)
+    {
 
-	public CSVReader(String data) {
-		ArrayList<Stop> crimeList = new ArrayList<Stop>();
-	}
-	
-	
-	
-}
+try
+         {
+             File dir = new File(
+                     "data");
+             for (File fn : dir.listFiles())
+             {
+                 BufferedReader br = new BufferedReader(new FileReader(fn));
+                 String strLine;
+                 while ((strLine = br.readLine()) != null)
+                 {
+                     System.out.println(strLine);
+                 }          
+                 br.close();
+             }
+
+         }
+         catch (FileNotFoundException e1)
+         {
+             e1.printStackTrace();
+         }
+         catch (IOException e)
+         {
+        	 e.printStackTrace();
+         }
+     }
+ }
+
