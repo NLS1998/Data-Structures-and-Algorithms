f45cf67ff500a9752e9115be03512eb013d0e6bb
Robert Rimmer <Robert Rimmer@LAPTOP-K9MUD67O>
2018-10-28 21:53:20 +0000

    importing all csv data from filewalk to string ready for splitting and storing in array list for sorting.

--- 5117 Stop and Search/src/main/CSVReader.java
+++ 5117 Stop and Search/src/main/CSVReader.java
@@ -1,7 +1,10 @@
 package main;
 
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -11,16 +14,28 @@ import java.nio.file.Paths;
 public class CSVReader {
     public static void main(String[] args) 
     {
-        // Directory path here
     	String rootPath = "data";
     	try {
 			Files.walk(Paths.get(rootPath)).filter(Files::isRegularFile).forEach(file -> {
 			    File csv = new File(file.toUri());
-			    //logic with file
+			    BufferedReader br = null;
+				try {
+					br = new BufferedReader(new FileReader(csv));
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();			} 
+    	  
+    	  String allData; 
+    	  try {
+			while ((allData = br.readLine()) != null) 
+			    System.out.println(allData);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
 			    System.out.println(csv.getAbsolutePath());
 			});
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+    	 
 }
 }
