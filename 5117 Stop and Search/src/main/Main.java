ee336d21fbbee7435452ad1bf40d5edff5470eb8
Robby McCammon <r.s.mccammon@2017.ljmu.ac.uk>
2018-11-07 17:46:59 +0000

    Left some comments in the main.java class describing what I did
    for Task C - Robby, Jack, Nick, Rob

--- 5117 Stop and Search/src/main/Main.java
+++ 5117 Stop and Search/src/main/Main.java
@@ -1,11 +1,9 @@
 package main;
 
 import java.io.FileNotFoundException;
-import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.Set;
 
 public class Main {
 
@@ -14,7 +12,7 @@ public class Main {
 		CSVReader reader = new CSVReader();// reads csvreader class
 		LinkedList<Stop> stopList = reader.createList();// Creates List
 		Scanner scan = new Scanner(System.in);
-
+		outputCrimes(stopList);
 		String choice = "";
 
 		for (Stop stop : stopList) {
@@ -58,4 +56,29 @@ public class Main {
 			System.out.println("-- GOODBYE --");
 		}
 	}
+
+	// Got this part in for task C however cannot get the successful part to work
+	// It constantly says that it cannot convert from an enum to a boolean
+	//However it is the True or False value from the data we need to sort this part
+	
+	private static void outputCrimes(List<Stop> stopList) {
+		int successful = 0, partSuccessful = 0, unsuccessful = 0;
+		for (int i = 0; i < stopList.size(); i++) {
+			Stop currentCrime = stopList.get(i);
+			if (currentCrime != null) {
+				if (currentCrime.outcomeObjectSearch.True)
+					successful++;
+				else if (currentCrime.outcome.matches("(A no|Nothing found).*"))
+					unsuccessful++;
+				else
+					partSuccessful++;
+
+			}
+		}
+		System.out.println("There are " + stopList.size() + " recorded crimes; ");
+		System.out.println(successful + " were successful");
+		System.out.println(partSuccessful + " were partially successful");
+		System.out.println(unsuccessful + " were unsuccessful");
+	}
+	
 }
