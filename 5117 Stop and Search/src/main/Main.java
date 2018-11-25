34236a90d8797d02f163106f283c1bf3af715d88
rob <rob@Omega>
2018-11-25 23:52:27 +0000

    trying to get the switch objectNo to work and accept different choice.

--- 5117 Stop and Search/src/main/Main.java
+++ 5117 Stop and Search/src/main/Main.java
@@ -18,18 +18,21 @@ public class Main {
 		outputCrimes(stopList);
 		Scanner scan = new Scanner(System.in);
 		String choice = "";
-		String choices = "";
+		String choices;
 		int index = 0;
-		
+
 		do {
 			System.out.println("\n** MAIN MENU **");
 			System.out.println("1 - Object Of Search + Relating data search."); // Feature A
-			
-			System.out.println("2 - Find and report the legislation which produces the highest Stop and Search frequency for a specified month ");
-			
-			System.out.println("3 - Find and report the legislation which produces the highest successful Stop and Search frequency for a specified month ");
-			
-			System.out.println("4 - Find which (self-determined) ethnic group has the highest number of recorded stop and search events, and output this data");
+
+			System.out.println(
+					"2 - Find and report the legislation which produces the highest Stop and Search frequency for a specified month ");
+
+			System.out.println(
+					"3 - Find and report the legislation which produces the highest successful Stop and Search frequency for a specified month ");
+
+			System.out.println(
+					"4 - Find which (self-determined) ethnic group has the highest number of recorded stop and search events, and output this data");
 
 			System.out.print("Pick : ");
 
@@ -39,83 +42,207 @@ public class Main {
 			case "1": { // loops through StopListArray and compares duplicates, if duplicate is found it
 						// is removed.
 
-				List<String> objectsOfSearch = new ArrayList<>(); //new arraylist to hold data from StopListArray while deleting 
-																  //Duplicates.
-				
+				List<String> objectsOfSearch = new ArrayList<>(); // new arraylist to hold data from StopListArray while
+																	// deleting
+																	// Duplicates.
+
 				System.out.println("\n Search Categories.\n");
 				for (int i = 0; i < stopListArray.size(); i++) {
 					String object = stopListArray.get(i).getObjectSearch();
 					if (!objectsOfSearch.contains(object))
 						objectsOfSearch.add(object);
 				}
-				
+
 				for (Iterator<String> iterate = objectsOfSearch.iterator(); iterate.hasNext(); index++) {
-				    String pos = iterate.next();
-				    System.out.println(index + ": " + pos);
+					String pos = iterate.next();
+					System.out.println(index + ": " + pos);
 				}
-				System.out.println("\n** Please enter the number of Object of Search you would like to view data on? **\n");
+				System.out.println(
+						"\n** Please enter the number of Object of Search you would like to view data on? **\n");
 				Scanner scans = new Scanner(System.in);
 				choices = scans.nextLine();
 				boolean listContainsInput = false;
-
-				for (int counter = 0; counter < stopListArray.size(); counter++) {
-					if (choices.equals(stopListArray.get(counter).getObjectSearch())) {
-						listContainsInput = true;
-						System.out.println("** " + stopListArray.get(counter).DatatoCSVString()); // if statments works
-					}
-				}
 				
-				if (!listContainsInput)
-					System.out.println("sorry chief");
+				int objectNo = 0;
+
+
+				switch (objectNo) {
+				case 0: {
+					choices = "Offensive weapons";
+					for (int counter = 0; counter < stopListArray.size(); counter++) {
+						if (choices.equals(stopListArray.get(counter).getObjectSearch())) {
+							listContainsInput = true;
+							System.out.println("** " + stopListArray.get(counter).DatatoCSVString()); // if statments
+																										// works
+						}
+					}}
+					break;
+					
+				case 1: 
+					choices = "Controlled drugs";
+					objectNo = 1;
+					for (int counter = 0; counter < stopListArray.size(); counter++) {
+						if (choices.equals(stopListArray.get(counter).getObjectSearch())) {
+							listContainsInput = true;
+							System.out.println("** " + stopListArray.get(counter).DatatoCSVString()); // if statments
+																										// works
+							
+						}
+					}
+					break;
+				case 2:
+					choices = "Article for use in theft";
+					for (int counter = 0; counter < stopListArray.size(); counter++) {
+						if (choices.equals(stopListArray.get(counter).getObjectSearch())) {
+							listContainsInput = true;
+							System.out.println("** " + stopListArray.get(counter).DatatoCSVString()); // if statments
+																										// works
+							
+						}
+					}
+					break;
+				case 3:
+					choices = "Stolen goods";
+					for (int counter = 0; counter < stopListArray.size(); counter++) {
+						if (choices.equals(stopListArray.get(counter).getObjectSearch())) {
+							listContainsInput = true;
+							System.out.println("** " + stopListArray.get(counter).DatatoCSVString()); // if statments
+																										// works
+							
+						}
+					}
+					break;
+				case 4:
+					choices = "Articles for use in criminal damage";
+					for (int counter = 0; counter < stopListArray.size(); counter++) {
+						if (choices.equals(stopListArray.get(counter).getObjectSearch())) {
+							listContainsInput = true;
+							System.out.println("** " + stopListArray.get(counter).DatatoCSVString()); // if statments
+																										// works
+							
+						}
+					}
+					break;
+				case 6:
+					choices = "Firearms";
+					for (int counter = 0; counter < stopListArray.size(); counter++) {
+						if (choices.equals(stopListArray.get(counter).getObjectSearch())) {
+							listContainsInput = true;
+							System.out.println("** " + stopListArray.get(counter).DatatoCSVString()); // if statments
+																										// works
+							
+						}
+					}
+					break;
+				case 7:
+					choices = "Evidence of offences under the Act";
+					for (int counter = 0; counter < stopListArray.size(); counter++) {
+						if (choices.equals(stopListArray.get(counter).getObjectSearch())) {
+							listContainsInput = true;
+							System.out.println("** " + stopListArray.get(counter).DatatoCSVString()); // if statments
+																										// works
+							
+						}
+					}
+					break;
+				case 8:
+					choices = "Fireworks";
+					for (int counter = 0; counter < stopListArray.size(); counter++) {
+						if (choices.equals(stopListArray.get(counter).getObjectSearch())) {
+							listContainsInput = true;
+							System.out.println("** " + stopListArray.get(counter).DatatoCSVString()); // if statments
+																										// works
+							
+						}
+					}
+					break;
+				case 9:
+					choices = "Crossbows";
+					for (int counter = 0; counter < stopListArray.size(); counter++) {
+						if (choices.equals(stopListArray.get(counter).getObjectSearch())) {
+							listContainsInput = true;
+							System.out.println("** " + stopListArray.get(counter).DatatoCSVString()); // if statments
+																										// works
+							
+						}
+					}
+					break;
+				case 10:
+					choices = "Goods on which duty has not been paid etc.";
+					for (int counter = 0; counter < stopListArray.size(); counter++) {
+						if (choices.equals(stopListArray.get(counter).getObjectSearch())) {
+							listContainsInput = true;
+							System.out.println("** " + stopListArray.get(counter).DatatoCSVString()); // if statments
+																										// works
+							
+						}
+					}
+					break;
+				default:
+					choices = "Invalid month";
+					listContainsInput = false;
+					break;
+
+				}
+				System.out.println(choices);
+
 				break;
 			}
-				
+
 			case "2": {
-				System.out.println("\n** Which month would you like to search for? **\n "); // asks for the month the user wants to see
+				System.out.println("\n** Which month would you like to search for? **\n "); // asks for the month the
+																							// user wants to see
 				String decision = scan.nextLine();
-				
+
 				List<String> A1 = new ArrayList<>();// Stores legislation categories.
 				List<String> A2 = new ArrayList<>();// Stores legislation data
 				int indexs;
 				for (int i = 0; i < stopListArray.size(); i++) {
-				String legislation = stopListArray.get(i).getLegislation();
-				String legislations = stopListArray.get(i).DatatoCSVString();
-				if (!A1.contains(legislation))
-					A1.add(legislation);
+					String legislation = stopListArray.get(i).getLegislation();
+					String legislations = stopListArray.get(i).DatatoCSVString();
+					if (!A1.contains(legislation))
+						A1.add(legislation);
 					A2.add(legislations);
-				indexs = i;
+					indexs = i;
 				}
 				System.out.println(A1);
 				System.out.println(A2);
-				// the data of both arrays stored and printing we need link each legislation to each legislation.
-				}	
+				// the data of both arrays stored and printing we need link each legislation to
+				// each legislation.
+			}
 				break;
 
 			case "3": {
-				
+
 				break;
 			}
-			
-			case "4": { 
-				// Find and report the legislation which produces the highest 			 Stop and Search frequency for a specified month
-				// Find and report the legislation which produces the highest SUCCESFULL Stop and Search frequency for a specified month
-				
+
+			case "4": {
+				// Find and report the legislation which produces the highest Stop and Search
+				// frequency for a specified month
+				// Find and report the legislation which produces the highest SUCCESFULL Stop
+				// and Search frequency for a specified month
+
 				break;
 			}
 			case "5": {
-				// Find which (self-determined) ethnic group has the highest number of recorded stop and search events, and output this data
+				// Find which (self-determined) ethnic group has the highest number of recorded
+				// stop and search events, and output this data
 				// List in reverse chronological order
-				
+
 				break;
 			}
-			case "6": { 
-				// One more analysis (search and/or sort) feature of your choosing that uses multiple search attributes
-				
+			case "6": {
+				// One more analysis (search and/or sort) feature of your choosing that uses
+				// multiple search attributes
+
 				break;
+
 			}
 			}
 		} while (!choice.equals("Q"));
 		System.out.println("-- GOODBYE --");
+
 	}
 
 	private static void outputCrimes(List<Stop> stopList) {
@@ -123,7 +250,7 @@ public class Main {
 		for (int i = 0; i < stopList.size(); i++) {
 			Stop currentCrime = stopList.get(i);
 			if (currentCrime != null) {
-				
+
 				if (currentCrime.getOutcome()
 						.matches("(Suspect|Offender|Local|Article|Arrest|Khat|Penalty|Community|Summons|Caution).*")
 						&& currentCrime.getOutcomeObjectSearch() == true) { // If outcome was related to object of
@@ -159,5 +286,5 @@ public class Main {
 		System.out.println(invalid + "   || Had no outcome");
 		System.out.println(stars);
 	}
-	
+
 }
